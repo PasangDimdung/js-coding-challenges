f56e519048e3be1573f84b5878cb1d998eae5e7e
PasangDimdung <pasandimdung045@gmail.com>
2025-11-23 13:05:21 +0545

    Fixed q3: for nan and boolean

--- javascript-basics/q3.js
+++ javascript-basics/q3.js
@@ -2,16 +2,17 @@ function filterTruthy(arr){
     const result = [];
     arr.map((item) => {
         const value = typeof item;
-        if (value != "object" || value != undefined)
+        if (value != "object" || value != undefined || value != NaN)
         {
             if (value === "string" && item != "") return result.push(item)
             if (value === "number" && item != 0) return result.push(item)
+            if (value === "boolean" && item === true) return result.push(item)
         }
     });    
     return result; 
 }
 
-var values = [null, undefined, 0, 1, "", "0", -1, 5, "Pasang"];
+var values = [null, undefined, 0, 1, "", "0", -1, 5, "Pasang", true, false, NaN];
 
 console.log(filterTruthy(values));
 
