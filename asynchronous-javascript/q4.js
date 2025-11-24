dfbabaff853c3e10c05f49a427c4a51d184bf284
PasangDimdung <pasandimdung045@gmail.com>
2025-11-24 15:23:19 +0545

    Solved q4: Implemented parallel fetch

---
+++ asynchronous-javascript/q4.js
@@ -0,0 +1,17 @@
+function getUsersAndPosts(){
+
+    const userPromise = fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json());
+
+    const postPromise = fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json());
+
+    Promise
+    .all([userPromise, postPromise])
+    .then(([userResult, postResult]) => {
+        console.log({...userResult, ...postResult});
+    });
+}
+
+
+getUsersAndPosts();
