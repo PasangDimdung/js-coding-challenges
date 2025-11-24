f1e092717f652ddafb637889a8e316e3cb2801ad
PasangDimdung <pasangdimdung045@gmail.com>
2025-11-24 22:21:00 +0545

    Solved q5: Implemented using installed package.

---
+++ es6-modules-npm/lodash.js
@@ -0,0 +1,5 @@
+// CommonJS
+const _ = require('lodash');
+
+const arr = [1, 2, 2, 3];
+console.log("Unique:", _.uniq(arr));
--- es6-modules-npm/main.js
+++ es6-modules-npm/main.js
@@ -3,9 +3,13 @@ import g from "./greet.js";
 
 import doNothing, * as Utils from "./utils.js";
 
+
+
 console.log(Utils.doSomething());
 console.log(doNothing());
 
 console.log(add(1,2));
 console.log(sub(4,2));
 console.log(g("Pasang"));
+
+
